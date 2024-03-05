d1fac49f784a70eeee482335504a76dc7f85f253
ovaomnis <ovaomnis@gmail.com>
2024-03-05 19:00:40 +0600

    initial commit

---
+++ src/test/java/kg/ows/shin/ShinApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.ows.shin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
